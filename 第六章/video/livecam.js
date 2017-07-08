637187a651b32a254a364bc710574fe18b418abd
lizhizhou <lzz@meteroi.com>
2017-07-08 15:32:13 +0800

    Fix

--- 第六章/video/livecam.js
+++ 第六章/video/livecam.js
@@ -1,47 +1,30 @@
-const LiveCam = require('livecam');
-const webcam_server = new LiveCam
+var LiveCam = require('livecam');
+var webcam_server = new LiveCam
 ({
-	// address and port of the webcam UI
+	// 网络摄像头UI网页地址与端口
 	'ui_addr' : '127.0.0.1',
-	'ui_port' : 11000,
+	'ui_port' : 8080,
 
 	// address and port of the webcam Socket.IO server
 	// this server broadcasts GStreamer's video frames
 	// for consumption in browser side.
+	// 基于Socket.IO的视频数据流websocket广播地址与端口
 	'broadcast_addr' : '127.0.0.1',
 	'broadcast_port' : 12000,
 
-	// address and port of GStreamer's tcp sink
+	// GStreamer的TCP视频流端口与地址
 	'gst_tcp_addr' : '127.0.0.1',
 	'gst_tcp_port' : 10000,
 	
-	// callback function called when server starts
+	// 服务器启动的回调函数
 	'start' : function() {
 		console.log('WebCam server started!');
 	},
 	
 	// webcam object holds configuration of webcam frames
 	'webcam' : {
-		
-		// should frames be converted to grayscale (default : false)
-		'grayscale' : true,
-		
-		// should width of the frame be resized (default : 0)
-		// provide 0 to match webcam input
-		'width' : 800,
-
-		// should height of the frame be resized (default : 0)
-		// provide 0 to match webcam input
-		'height' : 600,
-		
-		// should a fake source be used instead of an actual webcam
-		// suitable for debugging and development (default : false)
+		// 是否使用测试源，否则使用真实摄像头
 		'fake' : true,
-		
-		// framerate of the feed (default : 0)
-		// provide 0 to match webcam input
-		'framerate' : 25
 	}
 });
-
 webcam_server.broadcast();
