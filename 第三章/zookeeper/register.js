7611c553a2c9aa48e972c11b1125eb8567f8c834
lizhizhou <lzz@meteroi.com>
2017-10-05 12:26:29 +0800

    zookeeper bug fix

--- 第三章/zookeeper/register.js
+++ 第三章/zookeeper/register.js
@@ -1,15 +1,17 @@
 var zookeeper = require('node-zookeeper-client');
 var client = zookeeper.createClient('localhost:2181');
+var root = '/serviceA/';
 var path = '/serviceA/instance';
 client.once('connected', function () {
     console.log('Connected to the server.');
-    client.create(path, CreateMode.EPHEMERAL_SEQUENTIAL,function (error) {
+    client.create('/serviceA', function (error) {});
+    client.create(path, zookeeper.CreateMode.EPHEMERAL_SEQUENTIAL,function (error) {
         if (error) {
             console.log('Failed to create node: %s due to: %s.', path, error);
-        } else {sssssss
+        } else {
             console.log('Node: %s is successfully created.', path);
         }
-        client.close();
+        //client.close();
     });
 });
 client.connect();
