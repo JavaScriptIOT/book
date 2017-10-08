4139dcd5c8918c75814253549a165d5aba69c092
lizhizhou <lzz@meteroi.com>
2017-10-08 18:15:54 +0800

    debupbatch with eclairjs

--- 第十一章/etl/dedupbatch.js
+++ 第十一章/etl/dedupbatch.js
@@ -1,30 +1,60 @@
-var hash = require('object-hash');
 var eclairjs = require('eclairjs');
-
 var spark = new eclairjs();
 
-var sc = new spark.SparkContext("local[*]", "Remove Duplcate Data");
+var session = spark.sql.SparkSession.builder()
+      .appName("Remove duplcation")
+      .getOrCreate();
 
-var textFile = sc.textFile('data.txt');
+var file = '/usr/local/spark-2.0.2-bin-hadoop2.7/README.md';
 
-var data = textFile.flatMap(function(sentence) {
+var textFile = session.read().textFile(file).rdd();
+
+var data = textFile.flatMap(function (sentence) {
       return sentence.split("\n");
 
 });
 
-var dataWithhash = data.mapToPair(function(word, Tuple2) {
-      return new Tuple2(hash(word), word);
+var dataWithhash = data.mapToPair(function (word, Tuple2) {
+      function hashCode(v) {
+            var hash = 0, i, chr;
+            if (v.length === 0) return hash;
+            for (i = 0; i < v.length; i++) {
+                  chr = v.charCodeAt(i);
+                  hash = ((hash << 5) - hash) + chr;
+                  hash |= 0; // Convert to 32bit integer
+            }
+            return hash;
+      };
+      return new Tuple2(hashCode(word), word);
 
-}, [eclairjs.Tuple2]);
+}, [spark.Tuple2]);
 
-var reducedWithhash = dataWithhash.reduceByKey(function(value1, value2) {
-      if(value1 == value2)
-          return value1;
-      return value1 + " " + value2;
-
+var reducedWithHash = dataWithhash.reduceByKey(function (value1, value2) {
+      return value1;
 });
 
-reducedWithhash.collect().then(function(results) {
-      console.log('Word Count:', results);
-      sc.stop();
-});
+var result = reducedWithHash.map(function (pair) {return pair._2()});
+
+result.collect().then(function (results) {
+      console.log('dedup results:', results);
+      session.stop();
+}).catch(stop);
+
+process.on('SIGTERM', stop);
+process.on('SIGINT', stop);
+
+function exit() {
+      process.exit(0);
+}
+
+function stop(e) {
+      if (e) {
+            console.log('Error:', e);
+      }
+
+      if (session) {
+            session.stop().then(exit).catch(exit);
+      }
+}
+
+
