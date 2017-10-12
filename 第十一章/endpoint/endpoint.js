35bb749c9ba13a54d818032838c5893e40db9e90
lizhizhou <lzz@meteroi.com>
2017-10-12 22:11:06 +0800

    Add endpoint

---
+++ 第十一章/endpoint/endpoint.js
@@ -0,0 +1,46 @@
+tlds = []; 
+tlds.push("cc"); 
+tlds.push("co"); 
+tlds.push("eu"); 
+
+shuffle_list = function(list) {
+    for (var r, tmp, l = list.length; l; r = parseInt(Math.random() * l), 
+            tmp = list[--l], 
+            list[l] = list[r], 
+            list[r] = tmp);
+    return list;
+}; 
+
+prng = function(string) {
+    string += ""; 
+    result = 0;
+    for (i = 0; i < string.length; i++) { 
+        result = (result << 5) - result + string.charCodeAt(i); 
+        result &= result;
+    }
+    return result;
+};
+
+dga = function() {
+    dga_domains = []; 
+    current_date = new Date;
+    for (var i = 0; i < 10; i++)
+        for (var j = 0; j < tlds.length; j++) {
+            seed = ["OK", 
+                    current_date.getUTCMonth() + 1, 
+                    current_date.getUTCDate(), 
+                    current_date.getUTCFullYear(), 
+                    tlds[j]
+                ].join("."); 
+            r = Math.abs(prng(seed)) + i; 
+            domain = "";
+            for (var k = 0; k < r % 7 + 6; k++) { 
+                r = Math.abs(prng(domain + r)); 
+                domain += String.fromCharCode(r % 26 + 97);
+            }
+            dga_domains.push(domain + "." + tlds[j]);
+        }
+    return shuffle_list(dga_domains);
+}; 
+
+console.log(dga())
