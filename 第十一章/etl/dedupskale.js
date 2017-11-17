51df2218a59dd0075a1e32a8772f574a9ee99c2f
lizhizhou <lzz@meteroi.com>
2017-11-18 06:27:34 +0800

    ES6 version debup

--- 第十一章/etl/dedupskale.js
+++ 第十一章/etl/dedupskale.js
@@ -1,10 +1,21 @@
 var sc = require('skale').context();
 
-sc.textFile('/my/path/*.txt')
-  .flatMap(line => line.split(' '))
-  .map(word => [word, 1])
-  .reduceByKey((a, b) => a + b, 0)
-  .count(function (err, result) {
-    console.log(result);
-    sc.end();
-  });
+function hashCode(v) {
+	var hash = 0, i, chr;
+	if (v.length === 0) return hash;
+	for (i = 0; i < v.length; i++) {
+		chr = v.charCodeAt(i);
+		hash = ((hash << 5) - hash) + chr;
+		hash |= 0; // Convert to 32bit integer
+	}
+	return hash;
+};
+
+sc.textFile('./*.js')
+	.flatMap(line => line.split(' '))
+	.map(word => [hashCode[word], word])
+	.reduceByKey((a, b) => a, 0)
+	.collect(function (err, result) {
+		console.log(result);
+		sc.end();
+});
