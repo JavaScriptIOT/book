5d2fb37103106bc01a3f55564845fcecfd46a6f1
lizhizhou <lzz@meteroi.com>
2017-11-25 12:50:34 +0800

    skale work fine

--- 第十一章/etl/dedupskale.js
+++ 第十一章/etl/dedupskale.js
@@ -1,21 +1,23 @@
-var sc = require('skale').context();
-
-function hashCode(v) {
-	var hash = 0, i, chr;
-	if (v.length === 0) return hash;
-	for (i = 0; i < v.length; i++) {
-		chr = v.charCodeAt(i);
-		hash = ((hash << 5) - hash) + chr;
-		hash |= 0; // Convert to 32bit integer
-	}
-	return hash;
-};
-
+var sc = require('skale-engine').context();
 sc.textFile('./*.js')
-	.flatMap(line => line.split(' '))
-	.map(word => [hashCode[word], word])
-	.reduceByKey((a, b) => a, 0)
+	.flatMap(function split(line) {
+		return line.split(' ')
+	})
+	.map(function map(word) {
+		function hashCode(v) {
+			var hash = 0, i, chr;
+			if (v.length === 0) return hash;
+			for (i = 0; i < v.length; i++) {
+				chr = v.charCodeAt(i);
+				hash = ((hash << 5) - hash) + chr;
+				hash |= 0; // Convert to 32bit integer
+			}
+			return hash;
+		};
+		return [hashCode(word), word]
+	})
+	.reduceByKey((a, b) => b, "")
 	.collect(function (err, result) {
 		console.log(result);
 		sc.end();
-});
+	});
