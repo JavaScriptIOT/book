9a0c96afa95106bff0c240ba2502ccb47c3b2fe6
lizhizhou <lzz@meteroi.com>
2017-11-25 11:11:24 +0800

    dedupelastic.js hash version

--- 第十一章/etl/dedupelastic.js
+++ 第十一章/etl/dedupelastic.js
@@ -1,15 +1,15 @@
 var kafka = require('kafka-node'),
-HighLevelConsumer = kafka.HighLevelConsumer,
-client = new kafka.Client();
+	HighLevelConsumer = kafka.HighLevelConsumer,
+	client = new kafka.Client();
 
 var elasticsearch = require('elasticsearch');
 var esclient = new elasticsearch.Client({
-  host: 'localhost:9200',
-  log: 'info'
+	host: 'localhost:9200',
+	log: 'info'
 });
 
 HighLevelConsumer = kafka.HighLevelConsumer,
-kafkaclient = new kafka.Client();
+	kafkaclient = new kafka.Client();
 
 var consumer = new HighLevelConsumer(
 	kafkaclient,
@@ -23,17 +23,29 @@ var consumer = new HighLevelConsumer(
 	}
 );
 
+function hashCode(v) {
+	var hash = 0, i, chr;
+	if (v.length === 0) return hash;
+	for (i = 0; i < v.length; i++) {
+		chr = v.charCodeAt(i);
+		hash = ((hash << 5) - hash) + chr;
+		hash |= 0; // Convert to 32bit integer
+	}
+	return hash;
+};
+
 consumer.on('message', function (kafkaMessage) {
 	console.log('Received key ' + kafkaMessage.key);
 	console.log('Received message ' + kafkaMessage.value);
 	var message = JSON.parse(kafkaMessage.value);
 	var key = hash(message);
 	client.create({
-	  index: 'message',        
-	  type: 'log',           
-	  id: key,                   
-	  body: message}, function (error, response) {
-	  if (error) console.log('send error')
+		index: 'debupelastic',
+		type: 'log',
+		id: hashCode(message),
+		body: message
+	}, function (error, response) {
+		if (error) console.log('send error')
 	});
 });
 
