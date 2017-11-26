abc3bee440ec7e151f6e87f4c4a1fa72b04b843c
lizhizhou <lzz@meteroi.com>
2017-11-26 15:27:14 +0800

    mqtt design

--- 第十一章/bridge/server.js
+++ 第十一章/bridge/server.js
@@ -1,7 +1,14 @@
+var kafka = require('kafka-node'),
+HighLevelConsumer = kafka.HighLevelConsumer,
+client = new kafka.Client();
+HighLevelProducer = kafka.HighLevelProducer,
+producer = new HighLevelProducer(client);
 var mosca = require('mosca');
 var moscaSettings = {
     port: 1883, //mosca (mqtt) port
 };
+var mqtt = require('mqtt')
+var client  = mqtt.connect('mqtt://localhost')
 
 var server = new mosca.Server(moscaSettings); //here we start mosca
 server.on('ready', setup); //on init it fires up setup()
@@ -11,10 +18,6 @@ function setup() {
    console.log('Mosca server is up and running')
 }
 
-
-var mqtt = require('mqtt')
-var client  = mqtt.connect('mqtt://test.mosquitto.org')
-
 client.on('connect', function () {
   client.subscribe('presence')
   client.publish('presence', 'Hello mqtt')
@@ -23,6 +26,34 @@ client.on('connect', function () {
 client.on('message', function (topic, message) {
   // message is Buffer
   console.log(message.toString())
+		var kafkaMessage = {
+			topic: 'mqtt',
+			key: topic,
+			messages : message.toString()
+		};
+		producer.send([kafkaMessage], function (err, data) {
+			if(err) {
+				console.log('Error sending data ' + err);
+			}
+		});
   client.end()
 })
 
+var consumer = new HighLevelConsumer(
+	client,
+	[{ topic: 'mqtt' }],
+	{
+		groupId: "mqttbridge",
+		autoCommit: true,
+		autoCommitIntervalMs: 5000
+	}
+);
+
+consumer.on('message', function (kafkaMessage) {
+	var message = kafkaMessage.value;
+	var key = kafkaMessage.key
+	console.log('Received key ' + kafkaMessage.key);
+	console.log('Received message ' + message);
+    client.subscribe(key)
+    client.publish(key, message)
+});
