a824ac05c0470e3cd589bd19f866ae6f8519eebb
lizhizhou <lzz@meteroi.com>
2017-12-04 20:58:47 +0800

    Add kerasjs model

--- 第十一章/security/security.js
+++ 第十一章/security/security.js
@@ -55,5 +55,29 @@ var model = new KerasJS.Model({
       filesystem: true
 
 })
+//model.ready()
+//    .then(() => {
+            // input data object keyed by names of the input layers
+        //     // or `input` for Sequential models
+        //         // values are the flattened Float32Array data
+        //             // (input tensor shapes are specified in the model config)
+        //                 const inputData = {
+//              'input_1': new Float32Array(data)
+//            }
 
-
+            // make predictions
+        //     return model.predict(inputData)
+        //       })
+        //         .then(outputData => {
+            // outputData is an object keyed by names of the output layers
+        //     // or `output` for Sequential models
+        //         // e.g.,
+        //             // outputData['fc1000']
+        //               })
+        //                 .catch(err => {
+            // handle error
+        //   })
+        //                 })
+        //         })
+        //                 }
+//    })
