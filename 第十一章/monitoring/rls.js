fee216f57efd9abb740084be6ba133ba104bcee0
lizhizhou <lzz@meteroi.com>
2017-12-05 11:30:07 +0800

    RLS first version

--- 第十一章/monitoring/rls.js
+++ 第十一章/monitoring/rls.js
@@ -1,10 +1,10 @@
 var math = require('mathjs');
-function finitQueue(size) {
+function FinitQueue(size) {
     this.size = size;
     this.queue = [];
 }
 
-finitQueue.prototype.push = function(e) {
+FinitQueue.prototype.push = function(e) {
     if (this.queue.length < this.size) {
         this.queue.push(e);
         return null;
@@ -14,23 +14,53 @@ finitQueue.prototype.push = function(e) {
     }
 } 
 
-var q = new finitQueue(2)
-q.push(1)
-q.push(2)
-q.push(3)
-q.push(4)
-console.log(q.queue)
-
+// Algorithm reference
 // https://en.wikipedia.org/wiki/Recursive_least_squares_filter
-
-var a = math.matrix([1, 4, 9, 16, 25]);
-
-function rls(size) {
+function RLS(size, lamda, delta) {
     this.size = size;
-    this.queue = finitQueue(size);
-    this.weight = new Array(size);
+    this.lamda = lamda;
+    this.delta = delta;
+    this.w = new Array(size);
+    this.P = math.multiply(math.eye(size), 1.0 / delta);
 }
 
-rls.prototype.enqueue = function (input) {
-    this.queue.push(input);
+RLS.prototype.init = function (w0, P0) {
+    this.w = w0;
+    this.P = P0;
 }
+
+RLS.prototype.update = function (x, d) {
+    var alpha = d -  math.multiply((math.transpose(x),this.w));
+    var g = math.multiply(math.multiply(this.P, x), 1 / (lamda +  math.multiply(math.multiply((math.transpose(x),this.P)), x)));
+    this.P = math.multiply(math.subtract(this.P, math.multiply(math.multiply((g, math.transpose(x))),this.P)),1/lamda); 
+    this.w = math.add(w,math.multiply(alpha,g)); 
+    return alpha;
+}
+
+RLS.prototype.estimate = function (x) {
+    return math.multiply(math.transpose(this.w),x);
+}
+
+function AnormalDetection(size, lamda, delta, threshold) {
+    this.queue = new FinitQueue(size);
+    this.rls   = new RLS(size,lamda,delta);
+    this.threshold = threshold;
+}
+
+AnormalDetection.prototype.detection = function (input) {
+    var x =   math.math.matrix(this.queue.queue);
+    var out = this.queue.push(input);
+    if(out != null) {
+        est = this.rls.estimate(x)
+        var error = this.rls.update(x, input);
+        if (math.abs(error) > this.threshold) console.log("Anormal found")
+    }
+}
+
+
+// var q = new finitQueue(2)
+// q.push(1)
+// q.push(2)
+// q.push(3)
+// q.push(4)
+// console.log(q.queue)
