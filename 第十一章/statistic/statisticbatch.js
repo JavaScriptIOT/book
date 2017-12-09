fd99135dcab7ea5f2eef57fedac9293885555c87
lizhizhou <lzz@meteroi.com>
2017-12-09 20:13:13 +0800

    statistic with error

--- 第十一章/statistic/statisticbatch.js
+++ 第十一章/statistic/statisticbatch.js
@@ -1,53 +1,43 @@
 var eclairjs = require('eclairjs');
 var spark = new eclairjs();
 
-var session = spark.sql.SparkSession.builder()
-      .appName("statistic batch")
-      .getOrCreate();
+var sc = new spark.SparkContext("local[*]", "Statistic batch");
 
-var file = '/usr/local/spark-2.0.2-bin-hadoop2.7/README.md';
+var event = [
+      "2017-07-15T12:42:04+11:00",
+      "2017-07-15T12:42:04+12:00",
+      "2017-07-15T12:42:04+13:00",
+      "2017-07-15T12:42:04+14:00",
+      "2017-07-15T12:42:04+15:00",
+      "2017-07-16T12:42:04+15:00",
+      "2017-07-16T12:42:04+15:00",
+      "2017-07-16T12:42:04+15:00",
+      "2017-07-16T12:42:04+15:00"
+]
 
-var textFile = session.read().textFile(file).rdd();
+var eventRDD = sc.parallelize(event);
 
-function timeWindow(timeStirng, window) {
-      var date = new Date(timeStirng);
-      date.setTime(date.getTime() - date.getTime() % window)
-      return date.toString()
-}
-var data = textFile.flatMap(function (sentence) {
-      return sentence.split("\n");
-}).map( function (timeString) {
-      return timeWindow(timeStirng,100) 
+var data = eventRDD
+.map(function (timeString) {
+      function timeWindow(timeString, window) {
+            var date = new Date(timeString);
+            date.setTime(date.getTime() - date.getTime() % window)
+            return date.toString()
+      }
+      return timeWindow(timeString, 20000)
 })
 
-var dataWithhash = data.mapToPair(function (timestamp, Tuple2) {
-      return new Tuple2(timestamp, 1);
-}, [spark.Tuple2]);
+ var dataPair = data.mapToPair(function (time, Tuple2) {
+       return new Tuple2(time, 1);
+ }, [spark.Tuple2]);
 
-var reduced = dataWithhash.reduceByKey(function (value1, value2) {
-      return value1;
+var reduced = dataPair.reduceByKey(function (value1, value2) {
+      return value1 + value2;
 });
 
-var result = reduced.map(function (pair) {return pair._2()});
+var result = reduced.map(function (pair) { return {time:pair._1(), statistic: pair._2()}});
 
 result.collect().then(function (results) {
-      console.log('statistic results:', results);
-      session.stop();
-}).catch(stop);
-
-process.on('SIGTERM', stop);
-process.on('SIGINT', stop);
-
-function exit() {
-      process.exit(0);
-}
-
-function stop(e) {
-      if (e) {
-            console.log('Error:', e);
-      }
-      if (session) {
-            session.stop().then(exit).catch(exit);
-      }
-}
-
+      console.log('Statistic results:', results);
+      sc.stop();
+});
