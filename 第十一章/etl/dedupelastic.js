a4843bdbf4c2439a0450d2482eed8f70e6223c3e
lizhizhou <lzz@meteroi.com>
2017-12-10 10:50:21 +0800

    debupes test ok

--- 第十一章/etl/dedupelastic.js
+++ 第十一章/etl/dedupelastic.js
@@ -1,7 +1,7 @@
 var kafka = require('kafka-node'),
 	HighLevelConsumer = kafka.HighLevelConsumer,
 	client = new kafka.Client();
-
+var hash  = require('object-hash');
 var elasticsearch = require('elasticsearch');
 var esclient = new elasticsearch.Client({
 	host: 'localhost:9200',
@@ -23,27 +23,15 @@ var consumer = new HighLevelConsumer(
 	}
 );
 
-function hashCode(v) {
-	var hash = 0, i, chr;
-	if (v.length === 0) return hash;
-	for (i = 0; i < v.length; i++) {
-		chr = v.charCodeAt(i);
-		hash = ((hash << 5) - hash) + chr;
-		hash |= 0; // Convert to 32bit integer
-	}
-	return hash;
-};
-
 consumer.on('message', function (kafkaMessage) {
 	console.log('Received key ' + kafkaMessage.key);
 	console.log('Received message ' + kafkaMessage.value);
-	var message = JSON.parse(kafkaMessage.value);
-	var key = hash(message);
-	client.create({
+	var message = kafkaMessage.value;
+	esclient.create({
 		index: 'debupelastic',
 		type: 'log',
-		id: hashCode(message),
-		body: message
+		id: hash(message.toString()),
+		body: {"message":message}
 	}, function (error, response) {
 		if (error) console.log('send error')
 	});
