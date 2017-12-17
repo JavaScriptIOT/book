55e08f4736cbdecd64233597fbbd60ee2ec94533
lizhizhou <lzz@meteroi.com>
2017-12-17 11:35:55 +0800

    Add mqtt to websocket Add Mofile App

--- 第六章/websocket/server.js
+++ 第六章/websocket/server.js
@@ -1,19 +1,61 @@
 var sys = require('sys');
 var net = require('net');
 var mqtt = require('mqtt');
-var io  = require('socket.io').listen(5000); //在5000端口启动socket.io服务
-var client = new mqtt.MQTTClient(1883, 'test.mosquitto.org', 'pusher');
-//启动MQTT客户端接受消息 
-io.sockets.on('connection', function (socket) { //当浏览器尝试链接时处理消息
-  socket.on('subscribe', function (data) {   //收到subscribe消息，操作mqtt客户端订阅响应的话题
-    console.log('Subscribing to '+data.topic);
-    client.subscribe(data.topic);
-  });
+var io = require('socket.io').listen(5000); 
+var client = mqtt.connect('mqtt://test.mosquitto.org')
+io.sockets.on('connection',  function (socket) { 
+      socket.on('subscribe',  function (data) {   
+            console.log('Subscribing to ' + data.topic);
+            client.subscribe(data.topic);
+        client.publish(data.topic, "27.0")
+      });
 });
- 
-client.addListener('mqttData', function(topic, payload){ //当mqtt客户端收到消息
-  sys.puts(topic+'='+payload);
-  io.sockets.emit('mqtt',{'topic':String(topic), 
-  'payload':String(payload)}); //向浏览器端发送mqtt消息并携带包含topic与paload的json字段
 
-});
+client.on('message', function (topic, message) { 
+      sys.puts(topic + '=' + message);
+      io.sockets.emit('mqtt', {
+        'topic': String(topic),
+          'payload': String(message)
+    }); 
+})
+
+var port = 8080
+var http = require("http"),
+    url = require("url"),
+    path = require("path"),
+    fs = require("fs");
+http.createServer(function (req, res) {
+    var pathname = __dirname + url.parse(req.url).pathname;
+    // response of web request
+    if (path.extname(pathname) == "") {
+        pathname += "/";
+    }
+    if (pathname.charAt(pathname.length - 1) == "/") {
+        pathname += "index.html";
+    }
+    fs.exists(pathname, function (exists) {
+        if (exists) {
+            switch (path.extname(pathname)) {
+                case ".html":
+                    res.writeHead(200, {
+                        "Content-Type": "text/html"
+                    });
+                    break;
+                default:
+                    res.writeHead(200, {
+                        "Content-Type": "application/octet-stream"
+                    });
+            }
+            fs.readFile(pathname, function (err, data) {
+                res.end(data);
+            });
+        } else {
+            res.writeHead(404, {
+                "Content-Type": "text/html"
+            });
+            res.end("<h1>404 Not Found</h1>");
+        }
+    });
+}).listen(port);
+
+console.log("Server running at port " + port);
